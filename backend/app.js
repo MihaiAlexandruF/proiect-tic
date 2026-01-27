83c6381e6ab75b2238d61e7ff1a3313ff3cfa8e1
mihai <mihai.4444@yahoo.com>
2026-01-27 18:44:03 +0200

    wew

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://your-project.vercel.app'
+    'https://proiect-tic-mocha.vercel.app'
 ];
 
 app.use(cors({
